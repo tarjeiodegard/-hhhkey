061087d18172811c5b9547e5da9672879caa065d
tarjeiodegard <32744018+tarjeiodegard@users.noreply.github.com>
2018-12-05 21:20:49 +0100

    Add files via upload

---
+++ Draw Out/sketch4.js
@@ -0,0 +1,20 @@
+var bg;
+
+function preload() {
+    bg = loadImage("assets/ghost.png");
+}
+
+function setup() {
+ 	createCanvas(windowWidth, windowHeight);  
+    background(bg);
+	strokeWeight(8)
+	stroke(160, 37, 137);
+}
+
+
+function touchMoved() {
+    
+	line(mouseX, mouseY, pmouseX, pmouseY);
+	return false;
+}
+
